7172af0608ca183b0f971d714bd8ed5977860f5c
Hugo Josefson <hugo@josefson.org>
2019-02-16 19:47:11 +0000

    Looks a little better.

--- example.js
+++ example.js
@@ -4,9 +4,21 @@ function noop() {
 }
 
 noop /*
-cat "$0"|awk "x==1{print}/\*\/$/{x=1}"|docker run --rm -i node;exit $?;
+cat "$0"|awk "x==1{print}/^\*\//{x=1}"|docker run --rm -i --init node node - "$0" "$@";exit $?;
 
-Single-file script runner via docker: https://github.com/hugojosefson/docker-shebang
+Single-file script runner via docker:
+https://github.com/hugojosefson/docker-shebang
 */
+process.argv.splice(1, 1) // Fixes arguments to be as expected.
 
-console.log('hello world')
+///////////////////////////////////////////////////////////////////////////////
+////  BEGIN YOUR SCRIPT HERE
+///////////////////////////////////////////////////////////////////////////////
+
+console.log('Hello world.')
+console.log(`
+I was called with arguments:
+${process.argv.map((arg, index) => `  $${index}: ${JSON.stringify(arg)}`)}
+`)
+
+process.exit(process.argv.length)
